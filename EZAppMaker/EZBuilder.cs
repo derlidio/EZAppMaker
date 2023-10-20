2d7face247c0a548e7b097535a6cbbce14decebb
derlidio <derlidio@gmail.com>
2023-10-20 14:19:21 -0300

    Added copyright branding.

--- EZAppMaker/EZBuilder.cs
+++ EZAppMaker/EZBuilder.cs
@@ -1,4 +1,14 @@
-﻿using EZAppMaker.Components;
+﻿/*__ ____  _             
+| __|_  / /_\  _ __ _ __ 
+| _| / / / _ \| '_ \ '_ \
+|___/___/_/ \_\ .__/ .__/
+|  \/  |__ _| |_|__|_| _ 
+| |\/| / _` | / / -_) '_|
+|_|  |_\__,_|_\_\___|_|
+ 
+(C)2022-2023 Derlidio Siqueira - Expoente Zero */
+
+using EZAppMaker.Components;
 
 namespace EZAppMaker
 { 
